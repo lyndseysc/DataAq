92a63b16a9a071817571109c815c1d6cc150e5d0
lyndseysc <35724145+lyndseysc@users.noreply.github.com>
2018-10-20 17:20:50 +0100

    Add files via upload

---
+++ muonmass.py
@@ -0,0 +1,33 @@
+#A code to read and sort muon pair mass data from the LHC
+
+#Import libraries
+
+import pylab
+import numpy as np
+
+#Read in data
+
+def read_file(file="upsilons-mass-xaa.txt"):
+  data = np.genfromtxt(file) 	#generates list of numbers from the text file
+  return data
+
+#Generate Histogram
+
+def histogram(data):
+	pylab.xlabel('Mass GeV/c^2')
+	pylab.ylabel('Frequency')
+	pylab.title('A histogram of muon pair mass from LHC data')
+	pylab.grid(True)
+	xmass = data
+	Nbins = 50
+	binMin = 8.45
+	binMax = 11.0
+	entries, binedges, patches = pylab.hist(xmass, bins = Nbins, range = [binMin, binMax])
+	pylab.savefig("MuonHistogram.pdf")
+	pylab.show()
+
+def main():
+  data = read_file()
+  histogram(data)
+
+main()
