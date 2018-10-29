839a0f8c779391256cda15cfb4141423fbe6e38c
Lyndsey <lyndsey.736@gmail.com>
2018-10-29 16:18:40 +0000

    now working for correct masses

--- muonmass.py
+++ muonmass.py
@@ -25,10 +25,11 @@ def histogram(data):
     Nbins = 50
     binMin = 8.45
     binMax = 11.0
-    entries, binedges, patches = pylab.hist(xmass, bins = Nbins, range = [binMin, binMax])
+    counts, binmass, patches = pylab.hist(xmass, bins = Nbins, range = [binMin, binMax])
     # maxima : use builtin function to find (max) peaks
     pylab.savefig("MuonHistogram.pdf")
     pylab.show()
+    return counts, binmass
 
 def histogram_low(data):
     pylab.xlabel('Mass GeV/c^2')
@@ -68,55 +69,43 @@ def histogram_high(data):
     entries, binedges, patches = pylab.hist(xmass, bins = Nbins, range = [binMin, binMax])
     pylab.show()
 
-def find_peaks(data):
+def find_peaks(counts, mass):
     #Largest peak between between 9.3 - 9.7Gev/c^2
     first_peak = []
-    for i in range (0, len(data)):
-        if data[i] > 9.4 and data[i] < 9.5:
-            first_peak.append(data[i])
-    a = np.histogram(first_peak)
-    #print(a)
-    b = np.split(a, [1])
-    first_peak_max = np.max(b[0])
-    first_peak_max_max = np.max(first_peak_max)
-    #print(b[0])
-    list = first_peak_max.tolist()
-    index = list.index(first_peak_max_max)
-    #print(first_peak_max)
-    #print(first_peak_max_max)
-    print(index)
-    xmassarray = b[1]
-    xmasslist = xmassarray.tolist()
-    print(xmasslist)
-    X_MASS_THE_CHEEKY_BASTARD = xmasslist[index]
-    print(X_MASS_THE_CHEEKY_BASTARD)
+    for i in range (0, len(mass)):
+        if mass[i] > 9.4 and mass[i] < 9.6:
+            if counts[i-1] <= counts[i] >= counts[i+1]:
+                first_peak.append(mass[i])
+    first_peak_max = np.max(first_peak)
     print("Length of first peak list is {}".format(len(first_peak)))
-    print("Max mass gamma(1S) is {}".format(np.max(b[0])))
-'''
+    print("Max mass gamma(1S) is {}".format(np.max(first_peak)))
+
     second_peak = []
-    for i in range (0, len(data)):
-        if data[i] > 9.9 and data[i] < 10.1:
-            second_peak.append(data[i])
+    for i in range (0, len(mass)):
+        if mass[i] > 9.9 and mass[i] < 10.2:
+            if counts[i-1] <= counts[i] >= counts[i+1]:
+                second_peak.append(mass[i])
     second_peak_max = np.max(second_peak)
     print("Length of second peak list is {}".format(len(second_peak)))
     print("Max mass gamma(2S) is {}".format(np.max(second_peak)))
 
     third_peak = []
-    for i in range (0, len(data)):
-        if data[i] > 10.3 and data[i] < 10.4:
-            third_peak.append(data[i])
+    for i in range (0, len(mass)):
+        if mass[i] > 10.3 and mass[i] < 10.4:
+            if counts[i-1] <= counts[i] >= counts[i+1]:
+                third_peak.append(mass[i])
     third_peak_max = np.max(third_peak)
     print("Length of third peak list is {}".format(len(third_peak)))
     print("Max mass gamma(3S) is {}".format(np.max(third_peak)))
 
     print("The difference between gamma(1S) and gamma(2S) is {}".format(np.max(second_peak) - np.max(first_peak)))
     print("The difference between gamma(1S) and gamma(3S) is {}".format(np.max(third_peak) - np.max(first_peak)))
-'''
+
 def main():
   data = read_file()
-  histogram(data)
+  counts, binmass = histogram(data)
   histogram_low(data)
   histogram_mid(data)
   histogram_high(data)
-  find_peaks(data)
+  find_peaks(counts, binmass)
 main()
